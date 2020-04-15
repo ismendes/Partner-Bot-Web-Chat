1ce48b36cd326f2b11796c67e87bb612136170b2
Isa Mendes <ismendes@microsoft.com>
2020-04-14 18:08:46 -0700

    PRoduction build

---
+++ precache-manifest.7b77f583b9fead24ce8b513368b5b2e5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff7168641fe3499bc53aeb6e677ca326",
+    "url": "/Partner-Bot-Web-Chat/index.html"
+  },
+  {
+    "revision": "bfddb22994560069ffc8",
+    "url": "/Partner-Bot-Web-Chat/static/css/main.4c8aacaa.chunk.css"
+  },
+  {
+    "revision": "f79ae3565e3bd30f889e",
+    "url": "/Partner-Bot-Web-Chat/static/js/2.361b8a47.chunk.js"
+  },
+  {
+    "revision": "ffb96c4a209f458eda301ec9e19e1551",
+    "url": "/Partner-Bot-Web-Chat/static/js/2.361b8a47.chunk.js.LICENSE"
+  },
+  {
+    "revision": "bfddb22994560069ffc8",
+    "url": "/Partner-Bot-Web-Chat/static/js/main.1a6ea521.chunk.js"
+  },
+  {
+    "revision": "cf33781608051297188e",
+    "url": "/Partner-Bot-Web-Chat/static/js/runtime-main.90ec038b.js"
+  }
+]);
